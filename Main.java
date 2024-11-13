0b723b2427b72f84f030da769146b9f0cfd483cb
Luciano Schiavone <luciano.schiavone@gmail.com>
2024-11-13 18:53:05 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Modello tshirt = new Modello("T-shirt", 7.50, 4.75);
         Modello camicia = new Modello("Camicia", 6.00, 5.00);
+        Modello polo = new Modello("Polo",8.00,5.00);
         Colore giallo = new Colore("Giallo");
         Colore rosso = new Colore("Rosso");
         Materiale cotone = new Materiale("Cotone", 15.75);
         Materiale seta = new Materiale("Seta",19.90);
         Capo capo = new Capo(tshirt, rosso, cotone);
         Capo capo1 = new Capo(camicia,giallo,seta);
+        Capo capo2 = new Capo(polo, giallo, cotone);
         Collezione collezione = new Collezione();
         
         collezione.add(capo);
