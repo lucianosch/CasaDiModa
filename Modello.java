77b9a813186e7cc039f445c1f214f56b52258fb8
Luciano Schiavone <luciano.schiavone@gmail.com>
2024-11-13 19:20:43 +0100

    Reformat Modello.java

--- Modello.java
+++ Modello.java
@@ -3,21 +3,25 @@ public class Modello{
     private double costoFisso;
     private double quantita;
 
-public Modello(String nome,double costoFisso, double quantita){
-    this.nome=nome;
-    this.costoFisso=costoFisso;
-    this.quantita=quantita;
-}
-public String getNome(){
-    return nome;
-}
-public double getCostoFisso(){
-    return costoFisso;
-}
-public double getQuantita(){
-    return quantita;
-}
-public String toString() {
+    public Modello(String nome,double costoFisso, double quantita){
+        this.nome=nome;
+        this.costoFisso=costoFisso;
+        this.quantita=quantita;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getCostoFisso(){
+        return costoFisso;
+    }
+
+    public double getQuantita(){
+        return quantita;
+    }
+
+    public String toString() {
         return "Modello: " + this.nome + ", quantita': " + this.quantita + ", costo: " + this.costoFisso;
     }
 }
